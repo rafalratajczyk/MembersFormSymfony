c406d49cc6bf9f5d3fdf57992f4130a5741d4bd4
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-12 14:00:05 +0200

    .

---
+++ MembersForm/src/AppBundle/Controller/SupportController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Form\Type\ContactFormType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SupportController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        $form = $this->createForm(ContactFormType::class);
+
+        return $this->render('support/index.html.twig', [
+            'my_form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @Route("/form-submission", name="handle-form-submission")
+     * @Method("POST)
+     */
+
+    public function handleFormSubmissionAction(Request $request)
+    {
+        $form = $this->createForm(ContactFormType::class);
+
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted() || !$form->isValid()) {
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        $data = $form->getData();
+
+        dump($data);
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Suport Form Submission')
+            ->setFrom($data['from'])
+            ->setTo('some email')
+            ->setBody(
+                $form->getData()['message'],
+                'text/plain'
+            );
+
+        $this->get('mailer')->send($message);
+    }
+}
