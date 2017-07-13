eafef7d3d11692dab68fccc2b4a060b394d4d05a
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-13 17:51:30 +0200

    Code of RegistrationController and schema update

--- MembersForm/src/AppBundle/Controller/RegistrationController.php
+++ MembersForm/src/AppBundle/Controller/RegistrationController.php
@@ -6,20 +6,45 @@ use AppBundle\Entity\Member;
 use AppBundle\Form\Type\MemberType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class RegistrationController extends Controller
 {
     /**
      * @Route("/register", name="registration")
      * @return \Symfony\Component\HttpFoundation\Response
+     * $throws \LogicException
      */
-    public function registrationAction()
+    public function registrationAction(Request $request)
     {
         $member = new Member();
 
         $form = $this->createForm(MemberType::class, $member, [
 
         ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $password = $this
+                ->get('security.password_encoder')
+                ->encodePassword(
+                    $member,
+                    $member->getPlainPassword()
+                );
+
+            $member->setPassword($password);
+
+            $em = $this->getDoctrine($password)->getManager();
+
+            $em->persist($member);
+            $em->flush();
+
+            $this->addFlash('success', 'You are now successfully registered');
+
+            return $this->redirectToRoute('homepage');
+        }
 
         return $this->render('registration/register.html.twig', [
             'registration_form' => $form->createView(),
