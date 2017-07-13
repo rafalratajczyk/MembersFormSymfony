005fd066ca2242474fa1a3120c4d824a976c1dc4
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-13 10:52:09 +0200

    Add support controller

---
+++ MembersForm/src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+
+    public function loginAction()
+    {
+        return $this->render('security/login.html.twig', [
+
+        ]);
+    }
+}
